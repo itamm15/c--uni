b01851f67fc6700ca6ff9e930c0d3ae395ba76c8
Mateusz <97304124+itamm15@users.noreply.github.com>
2024-05-24 20:41:11 +0200

    lab_1_zad_3

---
+++ lab_1_zad_3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ZadaniaNaStudia
+{
+    class Lab_1_zad_3
+    {
+        static void Main(string[] args)
+        {
+            decimal najmniejsza = 0, najwieksza = 0;
+            bool flag = true;
+
+            for (int index = 0; index < 5; index++)
+            {
+                flag = true;
+                Console.WriteLine($"Podaj {index + 1} liczbę");
+                while (flag)
+                {
+                    try
+                    {
+                        decimal liczba = Convert.ToDecimal(Console.ReadLine());
+
+                        if (index == 0)
+                        {
+                            najmniejsza = liczba;
+                            najwieksza = liczba;
+                            flag = false;
+                        }
+                        else
+                        {
+                            // sprawdz czy podana jest wieksza od poprzedniej
+                            if (liczba > najwieksza) najwieksza = liczba;
+                            if (liczba < najmniejsza) najmniejsza = liczba;
+                            flag = false;
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.WriteLine($"Nie udało się odczytać liczby, spróbuj ponownie podać {index + 1} liczbę!");
+                    }
+                }
+            }
+
+
+            Console.WriteLine($"Największa liczba spośród podanych wynosi: {najwieksza}");
+            Console.WriteLine($"Najmniejsza liczba spośród podanych wynosi: {najmniejsza}");
+        }
+    }
+}
+
