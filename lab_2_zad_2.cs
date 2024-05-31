046892c36bfbac33da9bd7855c6116b94a041f11
Mateusz <97304124+itamm15@users.noreply.github.com>
2024-05-31 17:36:33 +0200

    lab_2_zad_2

---
+++ lab_2_zad_2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ZadaniaNaStudia
+{
+    class Lab_2_zad_1
+    {
+        static void Main(string[] args)
+        {
+            int[] pierwszaTablica = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            int[] drugaTablica = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+
+            bool czyJestWTablicy = false;
+            int brakujacyElement = -1;
+
+            foreach (int element in pierwszaTablica) {
+                czyJestWTablicy = false;
+                foreach (int elementZDrugiej in drugaTablica)
+                {
+                    if (element == elementZDrugiej)
+                    {
+                        czyJestWTablicy = true;
+                        break;
+                    }
+                }
+
+                if (!czyJestWTablicy) brakujacyElement = element;
+            }
+
+            Console.WriteLine($"Znalazłem brakujący element, jest nim {brakujacyElement}!");
+        }
+    }
+}
